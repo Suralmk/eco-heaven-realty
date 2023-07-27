5ee7f0f1adba4cc01c1ff6612d7851418f8ee309
Suralmk <104755355+Suralmk@users.noreply.github.com>
2023-07-27 23:28:07 +0300

    search for homes

--- post/views.py
+++ post/views.py
@@ -3,6 +3,7 @@ from .models import *
 from django.contrib.auth import get_user_model
 from django.contrib import messages
 from .models import HomePost
+from authentication.templates.authentication import  *
 
 
 User = get_user_model()
@@ -14,3 +15,12 @@ def create_post(request):
         'homepost': postedHomes
     }
     return render(request, 'post/create_post.html', context)
+def search_post(request):
+    if request.method == 'POST':
+        searched = request.POST['searched']
+        searched_homes = HomePost.objects,filter(city__containes=searched)
+
+        context = {
+            'searched_homes':searched_homes
+        }
+        return render(request, 'post/seaarched_homes.html' )
